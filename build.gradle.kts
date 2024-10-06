b05378b573a50529116d8de77d9ccef7363e21c9
tarturr <tartur.dev@gmail.com>
2024-10-06 15:59:44 +0200

    Added Lombok dependency and fixed some syntax mistakes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,21 @@ repositories {
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
         url = uri("https://mvnrepository.com/artifact/com.zaxxer/HikariCP")
     }
 }
 
 dependencies {
     compileOnly("dev.folia:folia-api:1.20.6-R0.1-SNAPSHOT")
+
     implementation("com.zaxxer:HikariCP:6.0.0")
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 }
