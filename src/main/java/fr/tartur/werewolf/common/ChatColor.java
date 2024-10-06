efd2511f90f911b0b0bd8f9ef95b1627171c1f9d
tarturr <tartur.dev@gmail.com>
2024-10-06 17:59:14 +0200

    Added a new ChatColor enum, colors picked all by myself. :)

---
+++ src/main/java/fr/tartur/werewolf/common/ChatColor.java
@@ -0,0 +1,47 @@
+package fr.tartur.werewolf.common;
+
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import net.kyori.adventure.text.format.TextColor;
+
+@Accessors(fluent = true)
+@Getter
+public enum ChatColor {
+
+    LIGHT_RED(TextColor.color(0xFF7777)),
+    LIGHT_ORANGE(TextColor.color(0xFFBB77)),
+    LIGHT_YELLOW(TextColor.color(0xFFEE77)),
+    LIGHT_GREEN(TextColor.color(0x77FF77)),
+    LIGHT_CYAN(TextColor.color(0x77FFFF)),
+    LIGHT_BLUE(TextColor.color(0x77DDFF)),
+    LIGHT_INDIGO(TextColor.color(0x7777FF)),
+    LIGHT_PURPLE(TextColor.color(0xBB77FF)),
+    LIGHT_PINK(TextColor.color(0xFFAAFF)),
+
+    RED(TextColor.color(0xEE0000)),
+    ORANGE(TextColor.color(0xFF7700)),
+    YELLOW(TextColor.color(0xFFDF00)),
+    GREEN(TextColor.color(0x00DF00)),
+    CYAN(TextColor.color(0x00FFFF)),
+    BLUE(TextColor.color(0x00BBFF)),
+    INDIGO(TextColor.color(0x0000FF)),
+    PURPLE(TextColor.color(0xAA00FF)),
+    PINK(TextColor.color(0xFF77FF)),
+
+    DARK_RED(TextColor.color(0xBB0000)),
+    DARK_ORANGE(TextColor.color(0xDD7700)),
+    DARK_YELLOW(TextColor.color(0xEED000)),
+    DARK_GREEN(TextColor.color(0x0D000)),
+    DARK_CYAN(TextColor.color(0x00BBBB)),
+    DARK_BLUE(TextColor.color(0x0077BB)),
+    DARK_INDIGO(TextColor.color(0x0000BB)),
+    DARK_PURPLE(TextColor.color(0x8800D0)),
+    DARK_PINK(TextColor.color(0xDD00DD));
+
+    private final TextColor color;
+
+    ChatColor(TextColor color) {
+        this.color = color;
+    }
+
+}
