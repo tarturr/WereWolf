836aa4193952f7f464a8069e65b831bab2255e04
tarturr <tartur.dev@gmail.com>
2024-10-11 19:19:53 +0200

    Created a Clickable interface to better handle actions tied to any item, entity, block...

---
+++ src/main/java/fr/tartur/werewolf/gui/Clickable.java
@@ -0,0 +1,26 @@
+package fr.tartur.werewolf.gui;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+
+/**
+ * Class representing any action tied to an item.
+ * @param <T> The event which is triggered on the player's left/right click.
+ */
+public interface Clickable<T extends Event> {
+
+    /**
+     * The event of type {@code T} triggered when the item is left clicked.
+     * @param event The triggered event.
+     */
+    @EventHandler
+    void onLeftClick(T event);
+
+    /**
+     * The event of type {@code T} triggered when the item is right clicked.
+     * @param event The triggered event.
+     */
+    @EventHandler
+    void onRightClick(T event);
+
+}
