18b4ecfadcd656640d6f87164f9172099f96a938
tarturr <tartur.dev@gmail.com>
2024-10-13 12:23:19 +0200

    Moved the name field from the BaseCharacter class to the CharacterType enum.

--- src/main/java/fr/tartur/werewolf/common/game/characters/BaseCharacter.java
+++ src/main/java/fr/tartur/werewolf/common/game/characters/BaseCharacter.java
@@ -3,7 +3,10 @@ package fr.tartur.werewolf.common.game.characters;
 import fr.tartur.werewolf.common.game.GameInstance;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.UUID;
 
 /**
  * Class representing a playable character in the game.
@@ -13,38 +16,41 @@ import org.bukkit.entity.Player;
 public abstract class BaseCharacter {
 
     /**
-     * The character's raw name.
-     */
-    private final String name;
-
-    /**
      * The {@code CharacterType} associated with this character.
+     *
      * @see CharacterType
      */
-    private final CharacterType type;
+    protected final CharacterType type;
 
     /**
      * The {@code Player} which incarnates the character.
+     *
      * @see Player
      */
-    private final Player player;
+    protected final Player player;
 
     /**
      * The class constructor, only usable by its child classes which will have to set the character name as a
      * {@code String} and its type as a {@code CharacterType}.
-     * @param name The character's raw name.
+     *
      * @param type The character's type.
+     * @param uuid The {@code UUID} of the player playing as this character.
+     *
      * @see CharacterType
      */
-    protected BaseCharacter(String name, CharacterType type, Player player) {
-        this.name = name;
+    protected BaseCharacter(CharacterType type, UUID uuid) {
         this.type = type;
-        this.player = player;
+        this.player = switch (Bukkit.getPlayer(uuid)) {
+            case null -> throw new IllegalArgumentException("The player with the UUID " + uuid + " could not be found.");
+            case final Player foundPlayer -> foundPlayer;
+        };
     }
 
     /**
      * The method that will be called when it's the character's turn.
+     *
      * @param gameInstance The running game as a {@code GameInstance}.
+     *
      * @see GameInstance
      */
     public abstract void play(GameInstance gameInstance);
--- src/main/java/fr/tartur/werewolf/common/game/characters/CharacterType.java
+++ src/main/java/fr/tartur/werewolf/common/game/characters/CharacterType.java
@@ -12,19 +12,22 @@ import net.kyori.adventure.text.Component;
 @Accessors(fluent = true)
 public enum CharacterType {
 
-    VILLAGER(Component.text("Villageois").color(ChatColor.YELLOW.color())),
-    WEREWOLF(Component.text("Loup-Garou").color(ChatColor.DARK_RED.color())),
-    CUPID(Component.text("Cupidon").color(ChatColor.PINK.color())),
-    WITCH(Component.text("La Sorcière").color(ChatColor.PURPLE.color())),
-    LITTLE_GIRL(Component.text("La Petite Fille").color(ChatColor.BLUE.color())),
-    STEALER(Component.text("Le Voleur").color(ChatColor.DARK_CYAN.color())),
-    HUNTER(Component.text("Le Chasseur").color(ChatColor.GREEN.color())),
-    CLAIRVOYANT(Component.text("La Voyante").color(ChatColor.INDIGO.color())),
-    UNCHOSEN(Component.text("Non choisi").color(ChatColor.GRAY.color()));
+    CLAIRVOYANT("Voyante", Component.text("La Voyante").color(ChatColor.INDIGO.color())),
+    CUPID("Cupidon", Component.text("Cupidon").color(ChatColor.PINK.color())),
+    HUNTER("Chasseur", Component.text("Le Chasseur").color(ChatColor.GREEN.color())),
+    LITTLE_GIRL("La Petite Fille", Component.text("La Petite Fille").color(ChatColor.BLUE.color())),
+    STEALER("Le Voleur", Component.text("Le Voleur").color(ChatColor.DARK_CYAN.color())),
+    VILLAGER("Villageois", Component.text("Villageois").color(ChatColor.YELLOW.color())),
+    WEREWOLF("Loup-Garou", Component.text("Loup-Garou").color(ChatColor.DARK_RED.color())),
+    WITCH("La Sorcière", Component.text("La Sorcière").color(ChatColor.PURPLE.color())),
 
+    UNCHOSEN("Non choisi", Component.text("Non choisi").color(ChatColor.GRAY.color()));
+
+    private final String name;
     private final Component display;
 
-    CharacterType(Component display) {
+    CharacterType(String name, Component display) {
+        this.name = name;
         this.display = display;
     }
 
